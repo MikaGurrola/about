19f69bfbb709b4dfc36cb63b45337af13a9a1d6b
MikaGurrola <mikagurrola90@gmail.com>
2017-01-02 22:54:21 -0500

    added a sticky nav/removed footer, also added space before brackets

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,27 @@
-console.log("bye");
-
-
+// initialize Animate On Scroll elements
 AOS.init({
 	offset: 200,
 	duration: 1000,
 	easing: 'ease-in-sine',
 	delay: 100,
-	// disable: window.innerWidth < 600,
+	disable: window.innerWidth < 700
 });
 
-
+// setup sticky nav
+var nav = $('nav');
+var navHeight = $('nav').height();
+var navScrolled = 'nav-scrolled';
+var hero = $('.hero').height();
+var section = $('section');
+$(window).scroll(function() {
+	if( $(this).scrollTop() > hero ) {
+		nav.addClass(navScrolled);
+		section.css('margin-top', '66px');
+	} else {
+		nav.removeClass(navScrolled);
+		section.css('margin-top', '0px');
+	}
+});
 
 
 
